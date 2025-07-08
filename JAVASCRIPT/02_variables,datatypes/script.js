b53112576f923da0b7301e4019fb198007ebded5
amittdas <amitdas123456789@gmail.com>
2025-07-09 00:59:59 +0530

    Added VARIABLES & DATA TYPES

---
+++ JAVASCRIPT/02_variables,datatypes/script.js
@@ -0,0 +1,18 @@
+console.log("hey this is Amit")
+
+let a = 5;
+let b = 6;
+
+console.log(a+b)
+
+const c = 6;
+
+let x = "amit bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
