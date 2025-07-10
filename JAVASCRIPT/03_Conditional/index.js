dcde803b3b3b8f04e69664b102b84c4072a2545b
amittdas <amitdas123456789@gmail.com>
2025-07-10 17:07:25 +0530

    Added CONDITIONAL if else

---
+++ JAVASCRIPT/03_Conditional/index.js
@@ -0,0 +1,14 @@
+console.log("code is running")
+
+let age = 3;
+let grace = 2;
+
+console.log(age * grace);
+console.log(age * grace);
+
+if(age > 18){
+    console.log("you can drive");
+}
+else{
+    console.log("you cannot drive");
+}
