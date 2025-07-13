d30f5c49617af00917c2e0ae7f30b2c5cd3e5a0c
amittdas <amitdas123456789@gmail.com>
2025-07-13 13:37:07 +0530

    Added Selecting by ID, CLASSES

---
+++ JAVASCRIPT/13_Selecting by ids, Classes & more/script.js
@@ -0,0 +1,13 @@
+console.log("Amit")
+
+// let boxes = document.getElementsByClassName("box")
+// console.log(boxes)
+// boxes[2].style.backgroundColor = "red"
+
+document.getElementById("redbox").style.backgroundColor = "red"
+
+document.querySelector(".box").style.backgroundColor = "green"  //first box only
+
+// document.querySelectorAll(".box").forEach(e=>{
+//     e.style.backgroundColor = "green"
+// })
