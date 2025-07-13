bea4611b778d966190aa6f8b65b8b94dad5f5f93
amittdas <amitdas123456789@gmail.com>
2025-07-13 21:06:16 +0530

    Added INSERTING & REMOVING BY JS

---
+++ JAVASCRIPT/14_Inserting & removing elements using javascript/script.js
@@ -0,0 +1,9 @@
+document.querySelector(".box")
+document.querySelector(".container").innerHTML
+document.querySelector(".box").innerHTML
+// let div = document.createElement("div");
+// div.innerHTML = "I have been inserted <b>by harry</b>"
+// div.setAttribute("class", "created");
+// document.querySelector(".container").before(div);
+let cont = document.querySelector(".container")
+cont.insertAdjacentHTML("beforebegin", "<b> I am under the water. Please h elp me here too much raining.... iuuuuooooo</b>")
